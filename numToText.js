fa7a0d7531885c0c18a5632a60721f4a005e032d
Christine Chou <christinejchou@gmail.com>
2016-05-03 15:12:32 -0700

    Completed practice problem

--- numToText.js
+++ numToText.js
@@ -14,5 +14,36 @@ Ex: numToText("I have 5 dogs and 6 ponies"); // returns "I have five dogs and si
 */
 
 var numToText = function(str) {
+   var result = [];
+   if (typeof str === 'string') {
+      str = str.split(' ')
+   }
+   var numbers = {
+      1: 'one',
+      2: 'two',
+      3: 'three',
+      4: 'four',
+      5: 'five',
+      6: 'six',
+      7: 'seven',
+      8: 'eight',
+      9: 'nine'
+   }
 
+   if (str.length === 1) {
+      return str[0];
+   }
+   else {
+      var firstWord = str[0];
+      if (numbers.hasOwnProperty(firstWord)) {
+         result.push(numbers[firstWord]);
+         result = result.concat(numToText(str.slice(1)));
+      }
+      else {
+         result.push(str[0]);
+         result = result.concat(numToText(str.slice(1)));
+      }
+   }
+   return result;
 };
+console.log(numToText("I have 2 dogs and 5 cats"))
